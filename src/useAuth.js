2ab5c416645e3308efdcb5e0f314719695c4bec5
akutsang <akuutsang@gmail.com>
2024-11-18 09:00:07 +0100

    add duseAuth

--- src/useAuth.js
+++ src/useAuth.js
@@ -1,1 +1,51 @@
-export default function useAuth() {}
+import { useState, useEffect } from "react"
+import axios from "axios"
+import PropTypes from "prop-types";
+
+Dashboard.propTypes = {
+  code: PropTypes.string.isRequired,
+};
+
+
+export default function useAuth(code) {
+    const [accessToken, setAccessToken] = useState()
+    const [refreshToken, setRefreshToken] = useState()
+    const [expiresIn, setExpiresIn] = useState()
+    useEffect(() => {
+        axios
+        .post("http://localhost:3001/login", {
+            code,
+          })
+          .then(res => {
+            setAccessToken(res.data.accessToken)
+            setRefreshToken(res.data.refreshToken)
+            setExpiresIn(res.data.expiresIn)
+            window.history.pushState({}, null, "/")
+          })
+          .catch(() => {
+            window.location = "/"
+          })
+    }, [code])
+
+    useEffect(() => {
+        if (!refreshToken || !expiresIn) return
+        const interval = setInterval(() => {
+          axios
+            .post("http://localhost:3001/refresh", {
+              refreshToken,
+            })
+            .then(res => {
+              setAccessToken(res.data.accessToken)
+              setExpiresIn(res.data.expiresIn)
+            })
+            .catch(() => {
+              window.location = "/"
+            })
+        }, (expiresIn - 60) * 1000)
+    
+        return () => clearInterval(interval)
+      }, [refreshToken, expiresIn])
+    
+      return accessToken
+
+}
